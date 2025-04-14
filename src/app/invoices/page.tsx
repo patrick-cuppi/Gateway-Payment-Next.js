ecd881cbf4287f76b574ead3da632449d9b3d799
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-14 18:43:16 -0300

    feat(invoices): created pages of invoices

---
+++ src/app/invoices/page.tsx
@@ -0,0 +1,8 @@
+import { InvoiceList } from "@/app/invoices/invoice-list"
+
+export default function InvoiceListPage() {
+    // const isAuthenticated = checkAuth()
+    // if (!isAuthenticated) redirect('/auth')
+
+    return <InvoiceList />
+}
